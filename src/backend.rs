dec2d989c2147c942b5fc0183cd9ce82f22cdf78
Nils Koch <mail@nilskch.dev>
2025-04-27 13:49:36 +0200

    lsp: publish diagnostics for conflicts

--- src/backend.rs
+++ src/backend.rs
@@ -1,16 +1,57 @@
+use std::ops::Deref;
+use std::sync::Arc;
+
 use tower_lsp_server::jsonrpc::Result;
 use tower_lsp_server::lsp_types::*;
 use tower_lsp_server::{Client, LanguageServer};
 
 use crate::conflict::Analyzer;
+use crate::types::Conflict;
 
 pub struct Backend {
+    inner: Arc<BackendInner>,
+}
+
+impl Deref for Backend {
+    type Target = BackendInner;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+pub struct BackendInner {
     client: Client,
 }
 
 impl Backend {
     pub fn new(client: Client) -> Self {
-        Self { client }
+        Self {
+            inner: Arc::new(BackendInner { client }),
+        }
+    }
+
+    pub fn get_diagnostics_from_conflicts(&self, conflicts: &[Conflict]) -> Vec<Diagnostic> {
+        conflicts
+            .iter()
+            .flat_map(|conflict| {
+                let header_diagnostic = Diagnostic {
+                    message: "Found conflicting changes".to_string(),
+                    range: conflict.title_range,
+                    severity: Some(DiagnosticSeverity::ERROR),
+                    ..Default::default()
+                };
+
+                let block_diagnostics = conflict.blocks.iter().map(|conflict_block| Diagnostic {
+                    message: "Conflicting change".to_string(),
+                    range: conflict_block.title_range,
+                    severity: Some(DiagnosticSeverity::WARNING),
+                    ..Default::default()
+                });
+
+                block_diagnostics.chain(std::iter::once(header_diagnostic))
+            })
+            .collect()
     }
 }
 
@@ -21,7 +62,15 @@ impl LanguageServer for Backend {
                 text_document_sync: Some(TextDocumentSyncCapability::Kind(
                     TextDocumentSyncKind::FULL,
                 )),
-                ..ServerCapabilities::default()
+                diagnostic_provider: Some(DiagnosticServerCapabilities::Options(
+                    DiagnosticOptions {
+                        identifier: Some("jj".to_string()),
+                        inter_file_dependencies: false,
+                        workspace_diagnostics: false,
+                        work_done_progress_options: Default::default(),
+                    },
+                )),
+                ..Default::default()
             },
             ..InitializeResult::default()
         })
@@ -29,7 +78,12 @@ impl LanguageServer for Backend {
 
     async fn did_open(&self, params: DidOpenTextDocumentParams) {
         let mut analyzer = Analyzer::new(&params.text_document.text);
-        let _conflicts = analyzer.find_conflicts();
+        let conflicts = analyzer.find_conflicts();
+        let diagnostics = self.get_diagnostics_from_conflicts(conflicts);
+
+        self.client
+            .publish_diagnostics(params.text_document.uri, diagnostics, None)
+            .await
     }
 
     async fn did_change(&self, params: DidChangeTextDocumentParams) {
@@ -42,8 +96,14 @@ impl LanguageServer for Backend {
                 .await;
             return;
         };
+
         let mut analyzer = Analyzer::new(&content.text);
-        let _conflicts = analyzer.find_conflicts();
+        let conflicts = analyzer.find_conflicts();
+        let diagnostics = self.get_diagnostics_from_conflicts(conflicts);
+
+        self.client
+            .publish_diagnostics(params.text_document.uri, diagnostics, None)
+            .await
     }
 
     async fn shutdown(&self) -> Result<()> {
